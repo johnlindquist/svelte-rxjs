4268808e597bd11edc6bfd863e25f326003a7e9c
johnlindquist <johnlindquist@gmail.com>
2020-06-09 15:46:57 -0600

    Tue Jun  9 15:46:57 MDT 2020

---
+++ playlist.md
@@ -0,0 +1,7 @@
+
+### Hello World 
+
+[Go to branch](https://github.com/johnlindquist/svelte-rxjs/tree/hello-world) 
+
+[Edit on Codesandbox](https://codesandbox.io/s/github/johnlindquist/svelte-rxjs/tree/hello-world/?module=/src/index.js) 
+
--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,8 @@
 <script>
-  export let name
+  import { interval } from "rxjs";
+  import { map } from "rxjs/operators";
+
+  let timer = interval(1000).pipe(map(x => x * 2));
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to
-    learn how to build Svelte apps.
-  </p>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<main>{$timer}</main>
